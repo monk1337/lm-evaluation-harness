1a46b58671024ce1e68cbfa2df6e55ea09a5513d
daniel-furman <dryanfurman@gmail.com>
2024-04-04 23:13:14 -0700

    latest code

--- lm_eval/models/huggingface.py
+++ lm_eval/models/huggingface.py
@@ -695,10 +695,9 @@ class HFLM(LM):
         # TODO: raise an error if system prompt not compatible with template
         new_reqs = []
         for req in requests:
-            context, continuation = req.args[0].strip(), req.args[1].strip()
+            context, continuation = req.args[0].strip(), req.args[1]
 
             # regular zero-shot behavior
-            """
             chat = []
             if self.system_prompt is not None:
                 chat += [{"role": "system", "content": self.system_prompt}]
@@ -706,34 +705,7 @@ class HFLM(LM):
             chat += [
                 {"role": "user", "content": context},
             ]
-            """
 
-            # mmlu
-            try:
-                test = context.split("Answer:")
-                assert len(test) == 7
-                questions = []
-                answers = []
-                for itr, element in enumerate(test[:len(test)-1]):
-                    if itr == 0:
-                        questions.append(str(element).strip()) 
-                    else:
-                        answers.append(element[1])
-                        questions.append(str(element[2:]).strip())
-                chat = []
-                if self.system_prompt is not None:
-                  chat += [{"role": "system", "content": self.system_prompt}]
-                
-                for itr, question in enumerate(questions):
-                    chat += [
-                        {"role": "user", "content": question},
-                    ]
-                    if itr != len(questions)-1:
-                        chat += [
-                            {"role": "assistant", "content": answers[itr]},
-                        ]            
-            except:
-                print("FAILURE")
             # TODO: expose settings for chat formatting:
             # - whether some "trigger" / start of assistant response might be placed in assistant's generation for it
             # - if few-shot, should the fewshots be placed in separate convo turns? provided in user's single turn?...
