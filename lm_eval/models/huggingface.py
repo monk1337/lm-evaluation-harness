2f70c884283575be83073460f60515c19a890d25
Daniel Furman <dryanfurman@gmail.com>
2024-04-17 16:45:00 -0600

    Latest code

--- lm_eval/models/huggingface.py
+++ lm_eval/models/huggingface.py
@@ -503,12 +503,12 @@ class HFLM(LM):
             self._model = self.AUTO_MODEL_CLASS.from_pretrained(
                 pretrained,
                 revision=revision,
-                #torch_dtype=utils.get_dtype(dtype),
+                torch_dtype=utils.get_dtype(dtype),
                 trust_remote_code=trust_remote_code,
-                quantization_config=bnb_config,
-                torch_dtype=torch.bfloat16,
+                #quantization_config=bnb_config,
+                #torch_dtype=torch.bfloat16,
                 device_map="auto",
-                #**model_kwargs,
+                **model_kwargs,
             )
         else:
             try:
